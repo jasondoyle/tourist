8655b574fca3bc4089aa8c6766d236d512db500e
tomsteele <thomasjsteele@gmail.com>
2013-08-19 22:01:41 -0700

    Remove concurrency option, static value of 10

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,17 +5,12 @@
 var fs = require('fs');
 var async = require('async');
 var webshot = require('webshot');
-var Progress = require('progress');
 var request = require('request');
 var color = require('colors');
 
 var argv = require('optimist')
   .demand(1)
   .describe('c', 'Amount of concurrent requests')
-  .options('c', {
-           alias: 'concurrency',
-           default: 1000
-           })
   .describe('u', 'User-Agent string')
   .options('u', {
            alias: 'useragent',
@@ -41,6 +36,7 @@ var argv = require('optimist')
            alias: 'timeout',
            default: 10000
   })
+  .boolean('j')
   .describe('j', 'Output JSON object')
   .alias('j', 'json')
   .describe('o', 'Output file')
@@ -76,23 +72,24 @@ var urls = fs.readFileSync(argv._[0], {encoding: 'utf8'}).trim().split("\n");
 var results = [];
 
 // async queue
-var q = async.queue(getScreenShot, argv.c);
+var q = async.queue(getScreenShot, 10);
 q.push(urls);
 q.drain = complete;
 
-// progress bar
-var bar = new Progress('requests [:bar] :percent :etas'.green, { complete: '=', width: 20, total: urls.length });
+// progress bar 
+var pace = require('pace')(urls.length);
+
 function getScreenShot(url, cb) {
   var img = '';
   // request the page once first to get around phantomjs 401 bug
   var ropts = {'url': url, 'strictSSL': false, headers:{'User-Agent': argv.u}};
   request(ropts, function(err, res) {
-    // if response was not 401 grab a screenshot
-    if (!err && res.statusCode !== 401) {
+     // if response was not 401 grab a screenshot
+     if (!err && res.statusCode !== 401) {
       var href = res.request['uri'].href;
       webshot(href, options, function(err, rs) {
         if (err) {
-          bar.tick();
+          pace.op();
           cb();
         }
         else {
@@ -102,12 +99,12 @@ function getScreenShot(url, cb) {
               img += data.toString('base64');
             }
           });
-          rs.on('error', function(err) {
-            bar.tick();
+         rs.on('error', function(err) {
+            pace.op();
             cb();
           });
           rs.on('end', function() {
-            bar.tick();
+            pace.op();
             results.push({"url": url, "href": href, "img": img});
             cb();
           });
@@ -116,7 +113,7 @@ function getScreenShot(url, cb) {
     }
     // else if error or a 401 just skip it
     else {
-      bar.tick();
+      pace.op();
       cb();
     }
   });
@@ -144,12 +141,11 @@ function complete() {
 // output the results object as json
 function jsonOut(results) {
   var string = JSON.stringify(results, null, " ");
-  if (argv.o) {
+  if (argv.o !== 'index.html') {
     fs.writeFileSync(argv.o, string);
   }
   else if (argv.a) {
     fs.appendFileSync(argv.a, string);
-    process.exit(0);
   }
   else {
     console.log(string);
@@ -167,7 +163,9 @@ function htmlOut(results) {
 
   results.forEach(function(r) {
     string += '<p><a href="' + r.url + '">' + r.url + "</a></p>";
-    if (r.href != r.url) { string += '<p>=> <a href="' + r.href + '">' + r.href + "</a></p>"; }
+    if (r.href !== r.url) { 
+       string += '<p>=> <a href="' + r.href + '">' + r.href + "</a></p>"; 
+    }
     string += '<img class="outline" src="data:image/png;base64,' + r.img + '"/><br />';
   });
 
@@ -186,9 +184,6 @@ function htmlOut(results) {
 function checkArgs(argv) {
   if (!fs.existsSync(argv._[0])) {
     throw 'url file does not exist'.red;
-  }
-  if (isNaN(argv.c) || argv.c < 0) {
-    throw 'invalid concurrency amount given'.red;
   }
   if (argv.a && !fs.existsSync(argv.a)) {
     throw 'append file does not exist'.red;
