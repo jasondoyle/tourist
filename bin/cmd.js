ef9bd0501dad12886b594e0b0cdb95bb7cc35b7b
jasondoyle <doylej.ia@gmail.com>
2014-03-31 01:22:33 -0400

    read in client js at runtime

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var async = require('async');
 var webshot = require('webshot');
 var request = require('request');
+var path = require('path');
 var color = require('colors');
 var winston = require('winston');
 var checksum = require('checksum');
@@ -106,7 +107,7 @@ function profile(callback) {
         if (res.body.match(loginRegex)) {
           website.login = true;
         }
-        patterns.forEach(function(p) {
+        interestRegex.forEach(function(p) {
           if (res.body.match(p)) {
             website.interest += (res.body.match(p)).length;
           }
@@ -191,65 +192,8 @@ function jsonOut(results) {
 function htmlOut(results) {
   var css = '.outline {border: 1px solid black;} html,body {padding: 10px;}';
   var htmlData = '';
-  // embedded javascript
-  var js= ' function doSort(attr) { \
-              var sorted = []; \
-              var spanEls = document.getElementsByTagName("span"); \
-              var attrVals = getUniqueAttributes(attr, spanEls); \
-              if (attr == "interest") { \
-                attrVals.sort(function(a,b){return b-a}); \
-              } else { \
-                attrVals.sort(); \
-              } \
-              attrVals.forEach(function(val) { \
-                for (var i = 0; i < spanEls.length; i++) { \
-                  if (val == spanEls[i].getAttribute(attr)) { \
-                    sorted.push(spanEls[i]); \
-                  } \
-                } \
-              }); \
-              writeHtml(sorted); \
-            } \
-            function hideDups(checked) { \
-              var spanEls = document.getElementsByTagName("span"); \
-              var attrVals = getUniqueAttributes("checksum", spanEls); \
-              var unique = []; \
-              if (checked) { \
-                attrVals.forEach(function(val) { \
-                  for (var i = 0; i < spanEls.length; i++) { \
-                    var elVal = spanEls[i].getAttribute("checksum"); \
-                    if (val == elVal) { \
-                      if (unique.indexOf(elVal) > -1) { \
-                        spanEls[i].style.display = "none"; \
-                      } else { \
-                        unique.push(elVal); \
-                      } \
-                    } \
-                  } \
-                }); \
-              } else { \
-                for (var i = 0; i < spanEls.length; i++) { \
-                  spanEls[i].style.display = ""; \
-                } \
-              } \
-            } \
-            function getUniqueAttributes(attr, spanEls) { \
-              var vals = []; \
-              for (var i = 0; i < spanEls.length; i++) { \
-                vals.push(spanEls[i].getAttribute(attr)); \
-              } \
-              function onlyUnique(value, index, self) { \
-                return self.indexOf(value) === index; \
-              } \
-              return vals.filter(onlyUnique); \
-            } \
-            function writeHtml(spanEls) { \
-              var parent = document.getElementById("container"); \
-              parent.innerHtml = ""; \
-              spanEls.forEach(function(span) { \
-                parent.appendChild(span); \
-              }); \
-            }';
+  var jsTemplate = path.join(path.dirname(require.main.filename), '../lib/client.js');
+  var js = fs.readFileSync(jsTemplate, { encoding: 'utf8' });
 
   if (!argv.a) {
     htmlData += '<html lang="en"><head><style>' + css + '</style>';
